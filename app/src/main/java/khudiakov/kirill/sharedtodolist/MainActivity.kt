2552f45a246620cd9730646114a76cc23e51e17b
Kirill Khudiakov <khudiakovk@gmail.com>
2019-10-17 21:48:42 +0700

    implement google authentification

--- app/src/main/java/khudiakov/kirill/sharedtodolist/MainActivity.kt
+++ app/src/main/java/khudiakov/kirill/sharedtodolist/MainActivity.kt
@@ -1,12 +1,47 @@
 package khudiakov.kirill.sharedtodolist
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        startAuth()
+    }
+
+    private fun startAuth() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Successfully logged in", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 123
     }
 }
