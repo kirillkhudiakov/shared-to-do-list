6a20057dc700e83149008355474abf8dd766f037
Kirill Khudiakov <khudiakovk@gmail.com>
2019-10-17 22:36:58 +0700

    check if user has already logged in

--- app/src/main/java/khudiakov/kirill/sharedtodolist/MainActivity.kt
+++ app/src/main/java/khudiakov/kirill/sharedtodolist/MainActivity.kt
@@ -10,14 +10,14 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = FirebaseAuth.getInstance()
-        startAuth()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            startAuth()
+        }
     }
 
     private fun startAuth() {
